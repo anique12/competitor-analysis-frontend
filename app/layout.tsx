6c30d9528c772aac853c0e8c88f41a8d1a91a7aa
Anique <muhammad.anique@alexatranslations.com>
2025-01-08 11:19:18 +0500

    Formate

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { SidebarProvider } from '@/components/ui/sidebar';
 import { StoreProvider } from '@/store/StoreProvider';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import React from 'react';
 import './globals.css';
 
 const geistSans = Geist({
